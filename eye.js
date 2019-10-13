c0cc8f38146f84b9af5d0188f731036b95b670b2
alhana-yo <beatriz.german.rengel@gmail.com>
2019-10-13 19:23:23 +0200

    Added functionality

--- eye.js
+++ eye.js
@@ -1,23 +1,28 @@
 const iris = document.getElementById('iris');
 const eye = document.getElementById('eye');
+//These are the scales for x and y axes, regarding the size of the eye 
 const SCALEX = innerWidth / 100;
 const SCALEY = innerHeight / 60;
 
-document.addEventListener('mousemove', moveEye);
+let interval = 1000;
 
+/**
+ * Calculate the coordinates inside the eye, using the scales 
+ * @param {*} event 
+ */
 function moveEye(event) {
   iris.style.left = (event.clientX / SCALEX) + 'px';
   iris.style.top = (event.clientY / SCALEY) + 'px';
 }
 
-let interval = 1000;
-
+/**
+ * Simulate the blink of the eye randomly
+ */
 setInterval(() => {
-  
   interval = Math.floor(Math.random() * 5);
-
   if(!interval) {
     eye.classList.toggle('blink');
   }
-
 }, 500);
+
+document.addEventListener('mousemove', moveEye);
